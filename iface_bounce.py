2533a92dfbfbe2cb51ff92fa188860d3ac2d8a96
Ben Russell <110110241+bruss22@users.noreply.github.com>
2026-02-13 08:20:47 -0600

    Create iface_bounce.py

---
+++ iface_bounce.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import ssl
+import requests
+import urllib3
+import time
+import json
+import datetime
+import time
+import random
+urllib3.disable_warnings()
+date = datetime.datetime.now()
+apiurl = 'https://xxx.xxx.xxx.xxx/api/v2/cmdb/system/interface/portxxx'
+apitoken = 'Your API token'
+headers = {'Authorization': "Bearer {}".format(apitoken)}
+interface = requests.get(apiurl, headers=headers, verify=False)
+results = interface.json()
+data = results.get('results')
+while True:
+    zzz = random.uniform(30, 180)
+    try:
+        for x in data:
+            if x['status'] == 'up':
+                print('Switching from up to down')
+                down = {'status':'down'}
+                auth_response = requests.put(apiurl, headers=headers, json=down, verify=False)
+                results = auth_response.json()
+                status = requests.get(apiurl, headers=headers, verify=False)
+                state = status.json()
+                data = state.get('results')
+                print(f'Status is now {data[0]['status']}')
+            if x['status'] == 'down':
+                print('Switching from down to up')
+                up = {'status': 'up'}
+                auth_response = requests.put(apiurl, headers=headers, json=up ,verify = False)
+                results = auth_response.json()
+                status = requests.get(apiurl, headers=headers, verify=False)
+                state = status.json()
+                data = state.get('results')
+                print(f'Status is now {data[0]['status']}')
+            print(f'Sleeping for {zzz}')
+            time.sleep(zzz)
+    except Exception as e:
+        print(e)
+
